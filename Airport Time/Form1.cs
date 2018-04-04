715ede96c1d76701a15403ec3621a4c0a7cd3364
antonproshkovsky <antonproshkovsky@gmail.com>
2018-04-04 18:31:18 +0300

    some fixes

--- Airport Time/Form1.cs
+++ Airport Time/Form1.cs
@@ -35,6 +35,7 @@ namespace Airport_Time
             {
                 Form2 form2 = new Form2();
                 this.Hide();
+		Console.WriteLine('fasdsa');
                 form2.Show();
             }
             else
