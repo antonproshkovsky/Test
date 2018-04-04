fab864b8d320bea4e4e2e65e7624488edbf7cdd4
artemkryga <krygaartem@gmail.com>
2018-04-04 18:43:30 +0300

    fiiix

--- Airport Time/Form1.cs
+++ Airport Time/Form1.cs
@@ -35,7 +35,7 @@ namespace Airport_Time
             {
                 Form2 form2 = new Form2();
                 this.Hide();
-		Console.WriteLine('fasddsadsasa');
+		Console.WriteLine('fasd7777777sa');
                 form2.Show();
             }
             else
