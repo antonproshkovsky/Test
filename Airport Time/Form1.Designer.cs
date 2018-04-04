d4b29f654969d774f34eef13a93de0e47a461134
artemkryga <krygaartem@gmail.com>
2018-04-04 19:40:44 +0300

    pofixil login

--- Airport Time/Form1.Designer.cs
+++ Airport Time/Form1.Designer.cs
@@ -73,9 +73,10 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Location = new System.Drawing.Point(102, 98);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.Size = new System.Drawing.Size(85, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Login";
+            this.label1.Text = "Поменял логин";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
             // 
             // label2
             // 
--- Airport Time/Form1.cs
+++ Airport Time/Form1.cs
@@ -54,5 +54,10 @@ namespace Airport_Time
         {
 
         }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
