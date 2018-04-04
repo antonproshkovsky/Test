384f18d5c34fa03e223f761552bb8a72fa7be54d
artemkryga <krygaartem@gmail.com>
2018-04-04 18:37:35 +0300

    some fix

--- Airport Time/Form1.cs
+++ Airport Time/Form1.cs
@@ -35,7 +35,7 @@ namespace Airport_Time
             {
                 Form2 form2 = new Form2();
                 this.Hide();
-		Console.WriteLine('fasddsadsasa');
+		Console.WriteLine('fasddsadsasa678');
                 form2.Show();
             }
             else
